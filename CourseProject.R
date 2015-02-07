065329e8619bd786075c4bba14301fb8a54e4cee
Andre Bandarra <andreban@Andres-MacBook-Pro.local>
2015-02-07 21:13:48 -0200

    Updated gitignore. Added comment to dataset source

--- CourseProject.R
+++ CourseProject.R
@@ -1,3 +1,4 @@
+#http://groupware.les.inf.puc-rio.br/har
 if (!file.exists("training.csv")) {
   download.file("https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv", "training.csv", method="curl")
 }
@@ -8,4 +9,6 @@ if (!file.exists("test.csv")) {
 training <- read.csv("training.csv");
 test <- read.csv("test.csv");
 
+dim(training)
 head(training, 10)
+summary(training)
